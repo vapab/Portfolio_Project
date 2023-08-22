c6360dbfdf3d6cf9d6e7d372c9ad304c44cdebfa
vapab <valameda15@gmail.com>
2023-08-22 11:32:05 -0500

    add app

---
+++ src/data/misc/django_init.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS notes_project_db;
+CREATE DATABASE notes_project_db;
+
+\c notes_project_db
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET default_tablespace = '';
+SET default_with_oids = false;
